df515014f9e12ba7ccc3d74c2bb52c943af3b4ab
Emre Aydın <47244852+allegres@users.noreply.github.com>
2020-12-26 17:10:53 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import Login from './components/Login'
+import Tab from './components/Tab'
 import Tracker from './components/Tracker'
+import AboutUs from './components/AboutUs'
 import 'react-native-gesture-handler';
 
 const Drawer = createDrawerNavigator();
@@ -13,9 +14,8 @@ export default function App() {
   return (
     <NavigationContainer >
         <Drawer.Navigator >
-          <Drawer.Screen name="Login" component={Login}/>
-          <Drawer.Screen name="Tracker" component={Tracker}/>
-          <Drawer.Screen name="Exit" component={Tracker}/>
+          <Drawer.Screen name="Giriş" component={Tab}/>
+          <Drawer.Screen name="Hakkımızda" component={AboutUs}/>
         </Drawer.Navigator>
       </NavigationContainer>
   );
